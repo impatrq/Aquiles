0280f4114b143e284b85b091432a16d014b20b63
Juan Montilla <Juanidequilmes@gmail.com>
2025-04-04 11:36:13 -0300

    creo ui de login screen

---
+++ app-movil/aquiles/.dart_tool/flutter_build/d09a09462fde09aeaef8ee4df70a7659/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/juanignaciomontilla/Apps/aquiles/.dart_tool/flutter_build/d09a09462fde09aeaef8ee4df70a7659/native_assets.yaml: 
---
+++ app-movil/aquiles/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:aquiles/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AquilesApp());
+}
+
+class AquilesApp extends StatelessWidget {
+  const AquilesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aquiles',
+      routes: {
+        '/': (context) => const LoginScreen(),
+      },
+    );
+  }
+}
